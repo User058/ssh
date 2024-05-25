c61f4a60f00bdbed592a63485d34c3c216b1cabd
User058 <110759720+User058@users.noreply.github.com>
2024-05-25 16:59:47 +0300

    Update ssh-vpn.sh

--- ssh/ssh-vpn.sh
+++ ssh/ssh-vpn.sh
@@ -237,12 +237,16 @@ socket = l:TCP_NODELAY=1
 socket = r:TCP_NODELAY=1
 
 [dropbear]
-accept = 222
-connect = 127.0.0.1:22
+accept = 445
+connect = 127.0.0.1:109
 
 [dropbear]
 accept = 777
 connect = 127.0.0.1:109
+
+[openssh]
+accept = 777
+connect = 127.0.0.1:443
 
 [ws-stunnel]
 accept = 2096
