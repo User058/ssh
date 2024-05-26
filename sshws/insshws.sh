bae74f32d930b7bf54dde343c0659507b8773c3d
User058 <110759720+User058@users.noreply.github.com>
2024-05-26 14:35:25 +0300

    Update insshws.sh

--- sshws/insshws.sh
+++ sshws/insshws.sh
@@ -11,7 +11,7 @@ chmod +x /usr/local/bin/ws-dropbear
 chmod +x /usr/local/bin/ws-stunnel
 
 #System Dropbear Websocket-SSH Python
-wget -O /etc/systemd/system/ws-dropbear.service https://raw.githubusercontent.com/User058/ssh/main/sshws/wsdropbear.service && chmod +x /etc/systemd/system/ws-dropbear.service
+wget -O /etc/systemd/system/ws-dropbear.service https://raw.githubusercontent.com/User058/ssh/main/sshws/ws-dropbear.service && chmod +x /etc/systemd/system/ws-dropbear.service
 
 #System SSL/TLS Websocket-SSH Python
 wget -O /etc/systemd/system/ws-stunnel.service https://raw.githubusercontent.com/User058/ssh/main/sshws/ws-stunnel.service && chmod +x /etc/systemd/system/ws-stunnel.service
